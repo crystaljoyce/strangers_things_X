e58dee331eed999f0b49803e1718643959104030
Crystal Joyce <crystaljoyce@me.com>
2021-01-27 20:32:30 -0800

    Post Component Help

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,42 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { 
-    Login, 
-    Register
-} from './components';
-import { 
-    BrowserRouter as Router,
-    Route, 
-    Link,
-    Switch
-} from 'react-router-dom';
 
-const [ posts, setPosts ] = useState([]);
 
-const Posts = () => {
 
-    useState(() => {
-        const fetchPosts = async () => { 
-            const response = await fetch('https://strangers-things.herokuapp.com/api/2010-CPU-RM-WEB-PT/posts');
-            const data = await response.json();
-            console.log(data)
-            setPosts(data);
-        }
-        fetchPosts();
-    }, []);
+
+const Posts = (  props ) => {
+    const { postsList } = props; 
+    if (!postsList) {
+        return <div> Not Here </div>
+    }
 
     return <>
         <h1> 
             Posts 
             </h1>
             {
-            posts.map(post => <div key={post.id}>
-                <h3>{ post.title }</h3>
-                <div>{ post.body }</div>
-            </div>)}
-            ); </> 
+            postsList.map((post) => {
+                return <>  
+                    <div key={post.id}>
+                        <h3>{ post.title }</h3>
+                        <div>{ post.body }</div>
+                    </div> 
+                </>
+            })}
+            </>
 
-            }
+                };
     
    
 export default Posts;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { 
     Login, 
-    Register
+    Register,
+    Posts
 } from './components';
 import { 
     BrowserRouter as Router,
@@ -17,28 +18,43 @@ const App = () => {
     const [token, setToken] = useState('');
     const [ user, setUser] = useState('');
     console.log('token: ', token);
+    const [ posts, setPosts ] = useState();
 
-    useEffect(() => {  
-        fetch('https://strangers-things.herokuapp.com/api/2010-CPU-RM-WEB-PT/users/me', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': 'Bearer { token }'
-            },
-            body: JSON.stringify( { token } )
-            } ), [token]})
-    return <div>
-        <Router>
-        <Login 
-            setToken={ setToken }/> 
-        <Register 
-            setToken={ setToken }/> 
-        <Posts /> 
-            </Router>
-    </div>
+    const fetchPosts = async () => { 
+        
+        const response = await fetch('https://strangers-things.herokuapp.com/api/2010-CPU-RM-WEB-PT/posts');
+        const data = await response.json();
+        console.log(data.data.posts)
+        setPosts(data.data.posts)
+        return data.data.posts
+    }
+   
+    useEffect(() => {
+        fetchPosts()
+        console.log('posts: ',)
+    }, []);
+
+   // useEffect(() => {  
+        //fetch('https://strangers-things.herokuapp.com/api/2010-CPU-RM-WEB-PT/users/me', {
+            //method: 'GET',
+            //headers: {
+              //'Content-Type': 'application/json',
+              //'Authorization': `Bearer ${ token }`
+           // },
+            //body: JSON.stringify( { token } )
+           // } ), [token]})
+    return (<div>
+        {
+        //<Login setToken={ setToken }/> 
+}       <Register setToken={ setToken }/> 
+        { console.log()}
+        <Posts postsList={posts}/>      
+        </div>)
 }
 
 ReactDOM.render (
-    <App />, 
+    <Router>
+    <App />
+    </Router>,
     document.getElementById('root')
 )
